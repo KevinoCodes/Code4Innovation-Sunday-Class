2ff9cd75a70e66b7f6b6ae7798f881d3b9b9bfc2
KevinoCodes <58299737+KevinoCodes@users.noreply.github.com>
2022-07-24 18:47:25 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+function calcMultiply() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    console.log(num1*num2)
+    document.getElementById("answer").innerHTML = num1*num2
+}
+
+function calcAddition() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) + parseInt(num2)
+
+}
+
+function calcSubtraction() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) - parseInt(num2)
+
+}
+
+function calcDivision() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) / parseInt(num2)
+
+}
+
+function calcExponent() {
+    console.log("hello world")
+    var num1 = document.getElementById("number1").value
+    var num2 = document.getElementById("number2").value
+    console.log(num1)
+    console.log(num2)
+    document.getElementById("answer").innerHTML = parseInt(num1) ** parseInt(num2)
+
+}
